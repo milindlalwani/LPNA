6b5791e9b3f1c03bfa9e8d0ab573fcbcc2cb3e2c
Milind Lalwani <82229994+milindlalwani@users.noreply.github.com>
2023-05-04 21:11:43 -0500

    Update qr_factorization.py

--- qr_factorization.py
+++ qr_factorization.py
@@ -1,8 +1,18 @@
 import numpy as np
-#Time Complexity: complexity is 2mn^2 − (2/3)n^3 flops -> O(n^3)
-flops
-#Householder transform implementation to be used in QR Factorization
+
 def householder(A):
+    """
+    Perform Householder transformation on a given vector A.
+
+    inputs
+    ------
+    A: an input vector
+
+    outputs
+    -------
+    v: a Householder vector
+    tau: a scalar
+    """
     alpha = A[0]
     s = np.power(np.linalg.norm(A[1:]), 2)
     v = A.copy()
@@ -11,38 +21,55 @@ def householder(A):
         tau = 0
     else:
         t = np.sqrt(alpha**2 + s)
-        if alpha <= 0: 
+        if alpha <= 0:
             v[0] = alpha - t
-        else: 
-            v[0] =  -s / (alpha + t)
+        else:
+            v[0] = -s / (alpha + t)
         numerator = v[0]**2
         denominator = s + v[0]**2
-        tau = numerator/denominator
+        tau = numerator / denominator
         v /= v[0]
     return v, tau
 
+
 def qr_factorization(A):
+    """
+    Compute the QR factorization of a matrix A.
+
+    inputs
+    ------
+    A: an input matrix with size m x n
+
+    outputs
+    -------
+    Q: an orthonormal matrix with size m x n
+    R: an upper triangular matrix with size n x n
+    """
     row, col = A.shape
     R = A.copy()
     Q = np.identity(row)
 
     for j in range(0, col):
-        #Use Householder Transformation
+        # Use Householder Transformation
         v, tau = householder(R[j:, j])
         H = np.identity(row)
         H[j:, j:] -= tau * v.reshape(-1, 1) @ v
         R = H @ R
         Q = H @ Q
-    return Q[:col].T, R[:col]
+    return Q[:, :col], R[:col]
 
 
 m = 4
 n = 3
-
 A = np.random.rand(4, 3)
+
 q, r = np.linalg.qr(A)
 Q, R = qr_factorization(A)
-print("Q: " + Q) 
-print("q: " + q)
-print("R: " + R)
-print("r: " + r)
+
+print("Q:\n", Q)
+print("q:\n", q)
+print("R:\n", R)
+print("r:\n", r)
+
+#Time Complexity: complexity is 2mn^2 − (2/3)n^3 flops -> O(n^3)
+#Householder transform implementation to be used in QR Factorization
